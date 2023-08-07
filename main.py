8e74def613bdb14d8d03c69e8f2293927a757110
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-06 22:25:54 -0500

    implemented send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
+from send_email import send_email
 
 api_key = 'bc0addffaf184d0c9d2263041c9511c0'
 url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bc0addffaf184d0c9d2263041c9511c0'
 
 
-request = requests.get(url)     # creates request object type
-content = request.json()    # creates it into a dictionary
+# creates request object type
+request = requests.get(url)
+
+# creates it into a dictionary
+content = request.json()
+
+# access article titles and description
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article["title"] and article["description"] is not None:
+        body = body + article['title'] + '\n' + article['description'] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
---
+++ send_email.py
@@ -0,0 +1,14 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = 'raphaelsebastien.evangelista@gmail.com'
+    password = 'jujverbpptodpplg'   # google app password
+    receiver = "raphaelsebastien.evangelista@gmail.com"
+    my_context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
